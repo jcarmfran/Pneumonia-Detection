830de25ffee45f5c241cbf68c90bcf760ce6a5fb
Francisco Carmona <jcarmfran@gmail.com>
2024-03-14 15:49:06 -0500

    Preparing data stage

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from pneuDetection import logger
 from pneuDetection.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from pneuDetection.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
 ### DATA INGESTION STAGE ###
 
-STAGE_NAME = "Data Ingestion Stage"
+STAGE_NAME = "DATA INGESTION"
 
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
@@ -12,5 +13,19 @@ try:
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
-        logger.exception(e)
-        raise e
+    logger.exception(e)
+    raise e
+
+
+### DATA PREPARATION STAGE ###
+
+STAGE_NAME = "DATA PREPARATION"
+
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
--- src/pneuDetection/config/configuration.py
+++ src/pneuDetection/config/configuration.py
@@ -1,7 +1,8 @@
 from pneuDetection.constants import *
 import os
 from pneuDetection.utils.common import read_yaml, create_directories, save_json
-from pneuDetection.entity.config_entity import (DataIngestionConfig)
+from pneuDetection.entity.config_entity import (DataIngestionConfig,
+                                                PrepareBaseModelConfig)
 
 
 class ConfigurationManager:
@@ -16,6 +17,7 @@ class ConfigurationManager:
         create_directories([self.config.artifacts_root]) # creating artifacts folder
 
 
+    ### INGESTION ###
     def get_data_ingestion_config(self) -> DataIngestionConfig: # DataIngestionConfig type: root_dir, source_URL, local_data_file, unzip_dir
         config = self.config.data_ingestion # extracting data_ingestion from config.yaml
 
@@ -29,3 +31,23 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+
+
+    ### PREPARATION ### 
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+        
+        create_directories([config.root_dir])
+
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES
+        )
+
+        return prepare_base_model_config
